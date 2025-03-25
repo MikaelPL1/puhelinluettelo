58af2aa7e6b6d43343746c02402c042985b8daa9
MikaelPL1 <mikaelpl151@gmail.com>
2025-03-25 14:09:40 +0200

    1.8 tehty

--- part1/1.6+/part1/src/App.jsx
+++ part1/1.6+/part1/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.all}</p>
+      <p>average {props.avg}</p>
+      <p>positive {props.positive} %</p>
+    </div>
+  );
+};
 
 const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
-  const avg = (good - bad) / (good + neutral + bad)
-  const positive = good * 100 / (good + neutral + bad)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const all = good + neutral + bad;
+  const avg = all === 0 ? 0 : (good - bad) / all; 
+  const positive = all === 0 ? 0 : (good * 100) / all; 
 
   return (
     <div>
@@ -16,14 +29,10 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {avg}</p>
-      <p>positive {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} avg={avg} positive={positive} 
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
