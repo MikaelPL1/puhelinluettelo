0f95410c80ed580a9110ddf00a8174feb6feb395
MikaelPL1 <mikaelpl151@gmail.com>
2025-08-13 14:13:56 +0300

    2.9,2.10 valmiita seuraavaksi 2c osio

--- FSO2/src/App.jsx
+++ FSO2/src/App.jsx
@@ -1,53 +1,64 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-1231-1244' },
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
+  const personsFiltteri =
+    filter.trim() === ''
+      ? persons
+      : persons.filter((p) =>
+          p.name.toLowerCase().includes(filter.toLowerCase())
+        );
 
   const addPerson = (event) => {
-    event.preventDefault() // estää sivun uudelleenlatauksen
-    const newPerson = { name: newName, number: newNumber }
-    const nameExists = persons.some(person => person.name === newName)
+    event.preventDefault();
+    const newPerson = { name: newName, number: newNumber };
+    const nameExists = persons.some((person) => person.name === newName);
     if (nameExists) {
-      alert(`${newName} is already added to phonebook`)
-      return
+      alert(`${newName} is already added to phonebook`);
+      return;
     }
-    setPersons(persons.concat(newPerson)) // lisätään uusi henkilö taulukkoon
-  }
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+    setNewNumber('');
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: 
-          <input 
-            value={newName}
-            onChange={(event) => setNewName(event.target.value)}
-          />
-          <div>
-            number: 
-            <input 
-              value={newNumber}
-              onChange={(event) => setNewNumber(event.target.value)}
-            />
-            </div>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+
+      <Filter
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+
+      <div>
+        <h2>Add a new</h2>
+      </div>
+
+      <PersonForm
+        onSubmit={addPerson}
+        newName={newName}
+        onNameChange={(e) => setNewName(e.target.value)}
+        newNumber={newNumber}
+        onNumberChange={(e) => setNewNumber(e.target.value)}
+      />
 
       <h2>Numbers</h2>
-      {persons.map((person, index) => (
-        <div key={index}>{person.name} {person.number} </div>
-      ))}
+      <Persons persons={personsFiltteri} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ FSO2/src/components/Filter.jsx
@@ -0,0 +1,13 @@
+const Filter = ({ value, onChange }) => {
+  return (
+    <form>
+      filter shown with
+      <input
+        value={value}
+        onChange={onChange}
+      />
+    </form>
+  );
+};
+
+export default Filter;
