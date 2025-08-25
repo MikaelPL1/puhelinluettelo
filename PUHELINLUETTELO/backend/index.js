115330581f5e40612175f14edccebc4ff0e228d3
MikaelPL1 <mikaelpl151@gmail.com>
2025-08-25 11:17:13 +0300

    3.10

--- PUHELINLUETTELO/backend/index.js
+++ PUHELINLUETTELO/backend/index.js
@@ -89,7 +89,7 @@ app.put('/api/persons/:id', (req, res) => {
 res.json(persons.find(p => p.id === id))
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
