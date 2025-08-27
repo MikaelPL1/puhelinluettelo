ae9d5c0f7bdc8b0b519dd5f58ab802c22d0c2b34
MikaelPL1 <mikaelpl151@gmail.com>
2025-08-27 11:03:37 +0300

    asiat toimii, operaatiot vaihdettu mongoon

--- PUHELINLUETTELO/backend/index.js
+++ PUHELINLUETTELO/backend/index.js
@@ -6,19 +6,10 @@ app.use(express.json())
 app.use(express.static('dist'))
 const Person = require('./models/person')
 
-
-
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
-
-let persons = [
-  { id: 1, name: 'Arto Hellas', number: '040-123456' },
-  { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-  { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-  { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' },
-]
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -27,35 +18,42 @@ app.get('/', (req, res) => {
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
     res.json(persons)
+    console.log(persons)
   })
 })
 
 
 app.get('/info', (req, res) => {
-  const date = new Date()
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people</p>` +
-    `<p>${date}</p>`
-  )
+  Person.countDocuments({})
+    .then(count => {
+      const date = new Date()
+      res.send(
+        `<p>Phonebook has info for ${count} people</p>
+         <p>${date}</p>`
+      )
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find((p) => p.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  Person.findById(req.params.id).then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
 })
+
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter((p) => p.id !== id)
-  res.status(204).end()
+  Person.findByIdAndDelete(req.params.id).then(person => {
+      if (person) {
+        res.status(204).end()
+      } else {
+        res.status(404).end()
+      }
+  })
 })
-
-const luoId = () => Math.floor(Math.random() * 100000)
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
@@ -64,35 +62,32 @@ app.post('/api/persons', (req, res) => {
     return res.status(400).json({ error: 'name or number missing' })
   }
 
-  if (persons.some((p) => p.name === body.name)) {
-    return res.status(400).json({ error: 'name must be unique' })
-  }
-
-  const person = {
-    id: luoId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
+  })
 
-  persons = persons.concat(person)
-  res.json(person)
-  console.log(person.name, person.number)
+  person.save().then(savedPerson => {
+      res.json(savedPerson)
+      console.log(savedPerson)
+    })
 })
 
+
 app.put('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const body = req.body
-
-  const existing = persons.find((p) => p.id === id)
-  if (!existing) {
-    return res.status(404).end()
-  }
-
-  persons = persons.map(p =>
-  p.id === id ? { ...p, name: body.name, number: body.number } : p
-)
-
-res.json(persons.find(p => p.id === id))
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name, number: req.body.number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      if (updatedPerson) {
+        res.json(updatedPerson)
+        console.log(updatedPerson)
+      } else {
+        res.status(404).end()
+      }
+    })
 })
 
 const PORT = process.env.PORT
