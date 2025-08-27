45b7e4b72a5f937ecc6e83ba5a106dedd43f3141
MikaelPL1 <mikaelpl151@gmail.com>
2025-08-27 11:30:13 +0300

    3.15, 3.16 tehty.

--- PUHELINLUETTELO/backend/index.js
+++ PUHELINLUETTELO/backend/index.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 const Person = require('./models/person')
 
 morgan.token('body', (req) => JSON.stringify(req.body))
@@ -15,11 +15,12 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
   Person.find({}).then(persons => {
     res.json(persons)
     console.log(persons)
   })
+  .catch(error => next(error))
 })
 
 
@@ -34,7 +35,7 @@ app.get('/info', (req, res) => {
     })
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id).then(person => {
       if (person) {
         res.json(person)
@@ -42,10 +43,11 @@ app.get('/api/persons/:id', (req, res) => {
         res.status(404).end()
       }
     })
+    .catch(error => next(error))
 })
 
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id).then(person => {
       if (person) {
         res.status(204).end()
@@ -53,9 +55,10 @@ app.delete('/api/persons/:id', (req, res) => {
         res.status(404).end()
       }
   })
+  .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   if (!body.name || !body.number) {
@@ -71,10 +74,11 @@ app.post('/api/persons', (req, res) => {
       res.json(savedPerson)
       console.log(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 
-app.put('/api/persons/:id', (req, res) => {
+app.put('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndUpdate(
     req.params.id,
     { name: req.body.name, number: req.body.number },
@@ -87,8 +91,30 @@ app.put('/api/persons/:id', (req, res) => {
       } else {
         res.status(404).end()
       }
+
     })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
